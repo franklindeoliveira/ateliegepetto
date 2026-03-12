464c797414b45ddf67329fef73ae5f9d66f26500
Franklin Oliveira <franklin.oliveira@assesso.com.br>
2026-03-12 20:11:58 -0300

    Site de teste.

--- rodape.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <div class="container">   
-        <img class="logo logoFooter" src="img/logo.png" alt="Logo Laços de Encantamento">
-        <ul class="social">
-            <li><a target="_blank" class="youtube" href="https://www.youtube.com/channel/UCiVmKurWXuDKy1uQvZFI2Lg">YouTube</a></li>
-            <li><a target="_blank" class="facebook" href="https://www.facebook.com/brincadeirasdebebelacos">Facebook</a></li>
-        </ul>
-    </div>
-</footer>
